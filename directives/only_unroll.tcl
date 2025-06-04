915558b11ba78ca2e6acfa0f2fcb8faf2228b2da
ltrnnhdng <129613373+ltrnnhdng@users.noreply.github.com>
2025-06-04 12:30:32 +0700

    Create only_unroll.tcl

---
+++ directives/only_unroll.tcl
@@ -0,0 +1,9 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_unroll "MatConv/Ker_Cols"
+set_directive_unroll "MatConv/Ker_Rows"
+set_directive_unroll "MatConv/Output_Rows"
+set_directive_unroll "MatConv/Output_Cols"
